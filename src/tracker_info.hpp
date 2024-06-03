f01f5ab4e47f549358de47cba61aa979bb9f41fd
William SERVATY <william.servaty@epita.fr>
2024-06-03 22:44:20 +0200

    fix: tracker_info include lib/nlohmann/json.hpp header

--- src/tracker_info.hpp
+++ src/tracker_info.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "nlohmann/json.hpp"
+#include "lib/nlohmann/json.hpp"
 #include <string>
 #include <vector>
 
@@ -21,33 +21,4 @@ class TrackerInfo {
     std::vector<std::string> peers;
 };
 
-TrackerInfo TrackerInfo::parse_tracker_response(nlohmann::json& tracker_response) {
-    TrackerInfo tracker_info;
-
-    tracker_info.interval = tracker_response["interval"];
-
-    auto peers = tracker_response["peers"];
-    if (!peers.is_string()) {
-        // TODO: error handling
-        return tracker_info;
-    }
-    auto peers_str = peers.get<std::string>();
-
-    size_t number_of_peers = peers_str.size() / 6;
-    for (size_t i = 0; i < number_of_peers; i++) {
-        std::string ip_raw = peers_str.substr(i * 6, 4);
-        std::string ip = std::to_string(static_cast<unsigned char>(ip_raw[0])) + "." +
-                         std::to_string(static_cast<unsigned char>(ip_raw[1])) + "." +
-                         std::to_string(static_cast<unsigned char>(ip_raw[2])) + "." +
-                         std::to_string(static_cast<unsigned char>(ip_raw[3]));
-        std::string port_raw = peers_str.substr(i * 6 + 4, 2);
-        uint16_t port =
-            (static_cast<uint16_t>(static_cast<unsigned char>(port_raw[0])
-                                   << 8)) |
-            static_cast<uint16_t>(static_cast<unsigned char>(port_raw[1]));
-        tracker_info.peers.push_back(ip + ":" + std::to_string(port));
-    }
-
-    return tracker_info;
-}
 }  // namespace bittorrent
